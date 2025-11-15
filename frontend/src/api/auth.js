9a7ea869e8ac92c5ecea93526ec1ef562938e0f5
kayaniherbsgit <kayaniherbs@gmail.com>
2025-11-15 20:53:33 +0300

    Kayani Herbs Backend connected to Render + Stable build

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://kayaniherbs-com-backend.onrender.com/api/auth",
 });
 
 // SIGNUP
